a51f91682e07912b4f01a66ffa5123b59dbc89fb
Luminix <76751932+Le-Arcanist95@users.noreply.github.com>
2024-09-17 20:37:57 -0400

    Updated tailwind config to extend font families for added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+            },
+            fontFamily: {
+                lexend: 'var(--font-lexend)',
+                lexendExa: 'var(--font-lexend-exa)',
+                geistSans: 'var(--font-geist-sans)',
+                geistMono: 'var(--font-geist-mono)',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
